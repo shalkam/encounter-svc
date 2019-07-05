09416f4ac91e2fbccf8d56097e2e321b6c741308
Mostafa Sholkamy <shalkamm@gmail.com>
2019-07-05 21:44:29 +0200

    Finished encounter model

--- graphql/resolvers/resolvers.js
+++ graphql/resolvers/resolvers.js
@@ -3,15 +3,15 @@ const { resolvers: primitiveTypes } = require('graphql-fhir-primitive-types')
 const resolvers = {
   ...primitiveTypes,
   Query: {
-    patient: (_, { id }, { prisma }, info) =>
-      prisma.query.patientModel({ where: { id } }, info),
-    patients: (_, args, { prisma, req }, info) => {
+    encounter: (_, { id }, { prisma }, info) =>
+      prisma.query.encounterModel({ where: { id } }, info),
+    encounters: (_, args, { prisma, req }, info) => {
       console.log(Object.keys(req.request.headers), req.request.headers.test)
-      return prisma.query.patientModelsConnection({}, info)
+      return prisma.query.encounterModelsConnection({}, info)
     }
   },
   Mutation: {
-    createPatient: (_, { data }, { prisma }, info) => {
+    createEncounter: (_, { data }, { prisma }, info) => {
       if (data.name) {
         data.name = {
           create: data.name.map(name => {
@@ -31,7 +31,7 @@ const resolvers = {
       if (data.identifier) data.identifier = { create: data.identifier }
       if (data.telecom) data.telecom = { create: data.telecom }
       if (data.address) data.address = { create: data.address }
-      return prisma.mutation.createPatientModel({ data }, info)
+      return prisma.mutation.createEncounterModel({ data }, info)
     }
   }
 }
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,9 +1,15 @@
 const path = require('path')
+const fs = require('fs')
 const { importSchema } = require('graphql-import')
 const { schema: directives } = require('graphql-fhir-directives')
 const { typeDefs: primitiveTypes } = require('graphql-fhir-primitive-types')
-const typeDefs = importSchema(path.resolve(__dirname, 'schema.graphql'), {
-  directives,
-  primitiveTypes
-})
+const schemaPath = path.resolve(__dirname, 'schema.graphql')
+// console.log(directives)
+const typeDefs = importSchema(
+  `${primitiveTypes}
+${fs.readFileSync(schemaPath)}`,
+  {
+    directives
+  }
+)
 module.exports = typeDefs

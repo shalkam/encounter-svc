7a70cb523aca62ffa1fd76cac84e60d87c0a4641
Mostafa Sholkamy <shalkamm@gmail.com>
2019-07-06 12:09:19 +0200

    [WIP] finishing slicing

--- graphql/resolvers/resolvers.js
+++ graphql/resolvers/resolvers.js
@@ -12,26 +12,28 @@ const resolvers = {
   },
   Mutation: {
     createEncounter: (_, { data }, { prisma }, info) => {
-      if (data.name) {
-        data.name = {
-          create: data.name.map(name => {
-            if (name.given) name.given = { set: name.given }
-            return name
-          })
-        }
-      }
-      if (data.mariatalStatus) {
-        if (data.mariatalStatus.coding) {
-          data.mariatalStatus.coding = { create: data.mariatalStatus.coding }
-        }
-        data.mariatalStatus = {
-          create: data.mariatalStatus
-        }
-      }
-      if (data.identifier) data.identifier = { create: data.identifier }
-      if (data.telecom) data.telecom = { create: data.telecom }
-      if (data.address) data.address = { create: data.address }
-      return prisma.mutation.createEncounterModel({ data }, info)
+      console.log(data)
+      return null
+      // if (data.name) {
+      //   data.name = {
+      //     create: data.name.map(name => {
+      //       if (name.given) name.given = { set: name.given }
+      //       return name
+      //     })
+      //   }
+      // }
+      // if (data.mariatalStatus) {
+      //   if (data.mariatalStatus.coding) {
+      //     data.mariatalStatus.coding = { create: data.mariatalStatus.coding }
+      //   }
+      //   data.mariatalStatus = {
+      //     create: data.mariatalStatus
+      //   }
+      // }
+      // if (data.identifier) data.identifier = { create: data.identifier }
+      // if (data.telecom) data.telecom = { create: data.telecom }
+      // if (data.address) data.address = { create: data.address }
+      // return prisma.mutation.createEncounterModel({ data }, info)
     }
   }
 }
